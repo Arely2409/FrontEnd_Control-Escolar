dd0aed14a87f69667e2d8920908254c1609354b9
Arely2409 <arely.aguilarfarias@utxicotepec.edu.mx>
2023-08-15 10:17:00 -0600

    first commit

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlumnosService } from 'src/app/services/alumnos.service';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+  constructor( public studentsService:AlumnosService){}
+
+  addStudent(form:NgForm){
+    this.studentsService.addStudents(form.value).subscribe(
+      res => {
+        this.studentsService.getAllStudents().subscribe(
+          res => {
+            this.studentsService.students = res;
+            console.log(res);
+          }, err => console.error(err)
+        )
+      }, err => console.error(err)
+      
+    )
+  }
+
+}
---
+++ src/app/services/alumnos.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Alumno } from '../models/Alumno';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumnosService {
+  URL_API = 'http://127.0.0.1:8000/';
+  students:Alumno []=[];
+  selectedStudent: Alumno ={
+    matricula: "",
+    nombre: "",
+    apellidos: "",
+    cuatrimestre: 0,
+    promedio: 0
+  }
+
+  constructor(private http:HttpClient) { }
+  getAllStudents(){
+    return this.http.get<Alumno[]>(this.URL_API+'getAll');
+  }
+  addStudents(alumno:Alumno){
+    return this.http.post(this.URL_API+'insert', alumno);
+  }
+
+  deleteStudent(matricula:String){
+    return this.http.delete(this.URL_API+'deleteOne/'+matricula);
+  }
+
+  
+
+}
